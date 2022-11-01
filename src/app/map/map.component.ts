78716b0d786451d5d89ad374cc8a6364f310ba81
Quốc Đại Đặng <quocdaidang@Quocs-MacBook-Pro.local>
2022-11-01 20:22:05 +0700

    map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  map!: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 37.75;
+  lng = -122.41;
+
+  constructor() { }
+
+  ngOnInit(): void {
+      this.map = new mapboxgl.Map({
+        accessToken: environment.mapbox.accessToken,
+        container: 'map',
+        style: this.style,
+        zoom: 13,
+        center: [this.lng, this.lat]
+    });
+    // Add map controls
+    this.map.addControl(new mapboxgl.NavigationControl());
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false 
+,
+mapbox: {
+  accessToken: 'pk.eyJ1IjoiZGFuZ3F1b2NkYWkiLCJhIjoiY2w5Y3BudjJxMG55ZTN1cDhuMHNsZ2ZweiJ9.uxF84VF-bl4ptTrZ2JB52A'
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
